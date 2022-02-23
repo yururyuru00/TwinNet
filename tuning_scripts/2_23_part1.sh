7446e13875ac1be2f563940dc3dcc30aec47e7ce
yururyuru00 <jima971123@gmail.com>
2022-02-23 21:10:02 +0900

    update

--- tuning_scripts/2_23_part1.sh
+++ tuning_scripts/2_23_part1.sh
@@ -1,73 +1,73 @@
 IFS_BACKUP=$IFS
 IFS=$'\n'
 ary=("
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_Cora' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_Cora' \
      'GCN_Cora.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=GAT_Cora' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GAT_Cora' \
      'GAT_Cora.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKGAT_Cora' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKGAT_Cora' \
      'JKGAT_Cora.jk_mode=choice(max,cat,lstm)' \
      'JKGAT_Cora.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinGAT_Cora' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinGAT_Cora' \
      'TwinGAT_Cora.n_layer=range(2,10)'
 
 
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_CiteSeer' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_CiteSeer' \
      'GCN_CiteSeer.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=GAT_CiteSeer' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GAT_CiteSeer' \
      'GAT_CiteSeer.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKGAT_CiteSeer' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKGAT_CiteSeer' \
      'JKGAT_CiteSeer.jk_mode=choice(max,cat,lstm)' \
      'JKGAT_CiteSeer.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinGAT_CiteSeer' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinGAT_CiteSeer' \
      'TwinGAT_CiteSeer.n_layer=range(2,10)'
 
 
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_PubMed' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_PubMed' \
      'GCN_PubMed.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=GAT_PubMed' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GAT_PubMed' \
      'GAT_PubMed.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKGAT_PubMed' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKGAT_PubMed' \
      'JKGAT_PubMed.jk_mode=choice(max,cat,lstm)' \
      'JKGAT_PubMed.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinGAT_PubMed' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinGAT_PubMed' \
      'TwinGAT_PubMed.n_layer=range(2,10)'
 
 
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_PPIinduct' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_PPIinduct' \
      'GCN_PPIinduct.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=GAT_PPIinduct' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GAT_PPIinduct' \
      'GAT_PPIinduct.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKGAT_PPIinduct' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKGAT_PPIinduct' \
      'JKGAT_PPIinduct.jk_mode=choice(max,cat,lstm)' \
      'JKGAT_PPIinduct.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinGAT_PPIinduct' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinGAT_PPIinduct' \
      'TwinGAT_PPIinduct.n_layer=range(2,10)'
 
 
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_Arxiv' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_Arxiv' \
      'GCN_Arxiv.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=GAT_Arxiv' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GAT_Arxiv' \
      'GAT_Arxiv.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKGAT_Arxiv' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKGAT_Arxiv' \
      'JKGAT_Arxiv.jk_mode=choice(max,cat,lstm)' \
      'JKGAT_Arxiv.n_layer=range(2,10)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinGAT_Arxiv' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinGAT_Arxiv' \
      'TwinGAT_Arxiv.n_layer=range(2,10)'
      ")
 
--- tuning_scripts/2_23_part2.sh
+++ tuning_scripts/2_23_part2.sh
@@ -1,31 +1,31 @@
 IFS_BACKUP=$IFS
 IFS=$'\n'
 ary=("
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_PPI' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_PPI' \
      'GCN_PPI.n_layer=range(2,8)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=SAGE_PPI' \
+     python3 train.py -m mlflow.runname=for_paper 'key=SAGE_PPI' \
      'SAGE_PPI.n_layer=range(2,8)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKSAGE_PPI' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKSAGE_PPI' \
      'JKSAGE_PPI.jk_mode=choice(max,cat,lstm)' \
      'JKSAGE_PPI.n_layer=range(2,8)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinSAGE_PPI' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinSAGE_PPI' \
      'TwinSAGE_PPI.n_layer=range(2,8)'
 
 
-     python3 train.py -m mlflow.runname=2_23 'key=GCN_Reddit' \
+     python3 train.py -m mlflow.runname=for_paper 'key=GCN_Reddit' \
      'GCN_Reddit.n_layer=range(2,4)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=SAGE_Reddit' \
+     python3 train.py -m mlflow.runname=for_paper 'key=SAGE_Reddit' \
      'SAGE_Reddit.n_layer=range(2,4)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=JKSAGE_Reddit' \
+     python3 train.py -m mlflow.runname=for_paper 'key=JKSAGE_Reddit' \
      'JKSAGE_Reddit.jk_mode=choice(max,cat,lstm)' \
      'JKSAGE_Reddit.n_layer=range(2,4)'
 
-     python3 train.py -m mlflow.runname=2_23 'key=TwinSAGE_Reddit' \
+     python3 train.py -m mlflow.runname=for_paper 'key=TwinSAGE_Reddit' \
      'TwinSAGE_Reddit.n_layer=range(2,4)'
      ")
 
